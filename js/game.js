692cfd0d689431f1a2e7b8e960776b9ff049b017
Ilya Merkulov <flappytonteam@gmail.com>
2024-06-20 20:52:45 +0300

    update

--- js/game.js
+++ js/game.js
@@ -221,16 +221,16 @@ function preload() {
 
     // Birds
     this.load.spritesheet(assets.bird.red, 'assets/bird-yellow-sprite.png', {
-        frameWidth: 80,
-        frameHeight: 53
+        frameWidth: 60,
+        frameHeight: 43
     })
     this.load.spritesheet(assets.bird.blue, 'assets/bird-yellow-sprite.png', {
-        frameWidth: 80,
-        frameHeight: 53
+        frameWidth: 60,
+        frameHeight: 43
     })
     this.load.spritesheet(assets.bird.yellow, 'assets/bird-yellow-sprite.png', {
-        frameWidth: 80,
-        frameHeight: 53
+        frameWidth: 60,
+        frameHeight: 43
     })
 
     // Numbers
